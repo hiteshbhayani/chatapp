2da7e2b342447219d1a21b611b5e6fa134538460
Hitesh Bhayani <hiteshbhayani28@gmail.com>
2018-09-28 15:56:36 +0530

    Added Server

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', function(socket){
+	
+  socket.username = "anonymous";
+
+  socket.emit("request", "createUsername");
+
+  socket.on('user_connect', function(name){
+	socket.username = name;
+	socket.broadcast.emit('notify',{"from" : socket.username, "notification" : "connected" });
+  });
+  
+  socket.on('message', function(text){
+	socket.broadcast.emit('message',{"from" : socket.username, "text":text});
+  });
+  
+  socket.on('disconnect', function(){
+	socket.broadcast.emit('notify',{"from" : socket.username, "notification" : "disconnected" });
+  });
+});
+
+http.listen(process.env.PORT || 8080, function(){
+  console.log('listening on *:' + process.env.PORT || 8080);
+});
