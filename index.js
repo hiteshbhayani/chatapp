537786fcfba91f6becf8444e841f77d88e510003
Hitesh Bhayani <hiteshbhayani28@gmail.com>
2018-09-28 16:59:28 +0530

    Updated Allow-Origin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://codepen.io/hiteshbhayani/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
